be6088a03bfd3ac97e7641b80bfaf075b2b2b8b4
NikeRodriguez <nike_1109@outlook.es>
2024-05-29 19:42:56 -0500

    Tema: Update with MQ

--- src/main/java/com/codigo/mspruebadatos/controller/PersonaController.java
+++ src/main/java/com/codigo/mspruebadatos/controller/PersonaController.java
@@ -1,13 +1,26 @@
 package com.codigo.mspruebadatos.controller;
 
 import com.codigo.mspruebadatos.response.Persona;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/v1/persona")
+@RequiredArgsConstructor
+@RefreshScope
 public class PersonaController {
+    @Value("${propiedad.test}")
+    private String valorPropiedad;
+
+    @GetMapping("/hola")
+    public ResponseEntity<String> getValor(){
+        return ResponseEntity.ok(valorPropiedad);
+    }
 
     @GetMapping("/infoPersona")
     public Persona getPersona(){
